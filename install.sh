7a9c175c77b8feb74799f08f5a212abe5926092c
Madeleine <32415621+SpyHoodle@users.noreply.github.com>
2022-02-22 16:48:17 +0000

    Inital upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Make all scripts executable
+echo "Making all scripts executable"
+chmod -R +x src/*
+
+# Copy all scripts to ~/.local/bin
+echo "Copying to ~/.local/bin"
+cp -r src/* ~/.local/bin
