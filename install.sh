1e199572865a1ac3f43ae9d109114604e3c85b0b
Madeleine <32415621+SpyHoodle@users.noreply.github.com>
2022-02-24 17:57:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 echo "Making all scripts executable"
 chmod -R +x src/*
 
+# Create ~/.local/bin if not already existing
+mkdir -p ~/.local/bin
+
 # Copy all scripts to ~/.local/bin
 echo "Copying to ~/.local/bin"
 cp -r src/* ~/.local/bin
