4709a9f0a44dd3f5aabfa0c2a13a2f12055bddf5
spy <spyhoodle@gmail.com>
2022-02-27 19:35:24 +0000

    add cl

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Make all scripts executable
-echo "Making all scripts executable"
-chmod -R +x src/*
-
 # Create ~/.local/bin if not already existing
 mkdir -p ~/.local/bin
 
 # Copy all scripts to ~/.local/bin
-echo "Copying to ~/.local/bin"
-cp -r src/* ~/.local/bin
+echo "[install.sh] making all scripts executable" && chmod -R +x src/* &&
+# Make all scripts executable
+	echo "[install.sh] copying to ~/.local/bin" && { cp -r src/* ~/.local/bin &&
+		# Finished, success
+		echo "[install.sh] successfully finished" ;} || { echo "[install.sh] failure, couldn't install" && exit 1 ;}
